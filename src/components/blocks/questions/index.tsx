4a3d11e75f65b4eaadd91e892c12396f87dd4123
leedongwook-kr <63283076+leedongwook-kr@users.noreply.github.com>
2024-02-27 23:27:05 +0900

    feat:사용자는 문항에 대한 답안을 4개 보기 중에 선택할 수 있다

--- src/components/blocks/questions/index.tsx
+++ src/components/blocks/questions/index.tsx
@@ -1,6 +1,19 @@
-import {FlatList, Text, View} from 'react-native';
+import {
+  Dimensions,
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {useRecoilValue} from 'recoil';
 import quizQuestionListState from '../../../recoil/quizes/selector';
+
+function shuffle(array: []) {
+  array.sort(() => Math.random() - 0.5);
+}
+
+const {width: screenWidth} = Dimensions.get('window');
 
 const Questions = () => {
   const questionList = useRecoilValue(quizQuestionListState);
@@ -10,12 +23,49 @@ const Questions = () => {
   return (
     <FlatList
       data={questionList}
-      scrollEnabled={false}
-      renderItem={({item: {question, incorrect_answers, correct_answer}}) => {
-        return <View></View>;
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      renderItem={({
+        item: {question, incorrect_answers, correct_answer},
+        index: questionIdx,
+      }) => {
+        const optionList = [...incorrect_answers, correct_answer];
+
+        return (
+          <View style={styles.questionPage}>
+            <Text style={styles.question}>
+              {`${questionIdx + 1}. ${question}`}
+            </Text>
+            {optionList.map((option, index) => {
+              return (
+                <TouchableOpacity key={`${question}_${option}`}>
+                  <Text style={styles.option}>
+                    {index + 1}. {option}
+                  </Text>
+                </TouchableOpacity>
+              );
+            })}
+          </View>
+        );
       }}
     />
   );
 };
 
 export default Questions;
+
+const styles = StyleSheet.create({
+  questionPage: {
+    width: screenWidth,
+    padding: 20,
+  },
+  question: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  option: {
+    marginVertical: 5,
+    fontSize: 16,
+  },
+});
