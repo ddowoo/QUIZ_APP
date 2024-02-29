04d402f1285353164f0b5391622d02b84131cf2a
leedongwook-kr <63283076+leedongwook-kr@users.noreply.github.com>
2024-02-29 23:26:32 +0900

    test: Home Component 버튼 이벤트에 따른 reocoil 상태값 변경 확인

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -9,13 +9,46 @@ import {
 } from '@testing-library/react-native';
 
 import 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
 
 // Note: test renderer must be required after react-native.
 import Home from '../src/components/screens/home';
 import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {RootStackParams} from '../src/navigations/rootScreens';
-import {RecoilRoot} from 'recoil';
+import {RecoilRoot, RecoilState, useRecoilValue} from 'recoil';
+import {quizConfigState} from '../src/recoil/quiz/atom';
 
-describe('퀴즈 시작 버튼 상태값 변경 후 퀴즈 호출 체크', () => {});
+const RecoilObserver = ({
+  states,
+  onChange,
+}: {
+  states: RecoilState<{
+    count: QuizCount;
+    level: QuizLevel;
+  }>;
+  onChange: jest.Mock;
+}) => {
+  const value = useRecoilValue(states);
+  useEffect(() => onChange(value), [onChange, value]);
+  return null;
+};
+
+test('Home Component Test', () => {
+  const onChange = jest.fn();
+  const {getAllByLabelText} = render(
+    <RecoilRoot>
+      <RecoilObserver states={quizConfigState} onChange={onChange} />
+      <Home navigation={undefined} route={{key: '', name: 'home'}} />
+    </RecoilRoot>,
+  );
+
+  const countBntList = screen.getAllByLabelText('count btn');
+  const levelBtnList = screen.getAllByLabelText('level btn');
+
+  fireEvent(countBntList[1], 'press');
+  fireEvent(levelBtnList[0], 'press');
+
+  expect(onChange).toHaveBeenCalledTimes(3);
+  expect(onChange).toHaveBeenCalledWith({count: 10, level: 'easy'});
+});
